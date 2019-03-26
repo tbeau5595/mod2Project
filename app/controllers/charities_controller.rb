390c8b65bffd31cf0046dd9c3d924e3bfb682c89
github username <github email address>
2019-03-26 09:52:33 -0500

    updates reward level

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,29 +1,9 @@
 class CharitiesController < ApplicationController
     def index
-        @charities = Charity.find(params[:id])
+        @charities = Charity.all
     end
 
     def show
         @charity = Charity.find(params[:id])
-    end
-
-    def new
-
-    end
-
-    def edit
-
-    end
-
-    def create
-
-    end
-
-    def update
-
-    end
-
-    def charity_params
-        params.require(:charity).permit(:name, :description)
     end
 end
--- app/controllers/user_rewards_controller.rb
+++ app/controllers/user_rewards_controller.rb
@@ -1,10 +1,24 @@
 class UserRewardsController < ApplicationController
+    
     def index
 
     end
 
     def show
-        @user_reward = UserReward.find(params[:id])
+        @user_reward = UserReward.find_by(user_id: @user.id)
+        total = 0
+        @user.donations.each do |donation|
+            total += donation.amount
+            if total <= 100
+                @user_reward.update(reward_id: 1)
+            elsif 100 < total <= 250
+                @user_reward.update(reward_id: 2)
+            elsif 250 < total <= 500
+                @user_reward.update(reward_id: 3)
+            elsif 500 < total
+                @user_reward.update(reward_id: 4)
+            end
+        end   
     end
 
     def new
